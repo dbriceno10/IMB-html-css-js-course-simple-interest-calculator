bed65568dfab0d052fc187b85d5b790d665f92cb
nombre <daniel.b100726@gmail.com>
2021-06-08 18:35:01 -0400

    Exercise 4 : Modify the javascript file.

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-// const principal = document.getElementById("principal").value;
-// const rate = document.getElementById("rate").value;
-// const years = document.getElementById("years").value;
-// const interest = principal * years * rate / 100
-// const year = new Date().getFullYear() + parseInt(years);
-
+//updateRate function, we obtain the rate by moving the selector and update it on the screen in real time
 const updateRate = () => {
     const rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText = rateval;
@@ -14,22 +9,22 @@ const compute = () => {
     const principal = document.getElementById("principal").value;
     const rate = document.getElementById("rate").value;
     const years = document.getElementById("years").value;
+    //Obtain the values of the different inputs from the DOM, and assign them to a variable each
     const interest = principal * years * rate / 100
+    //Calculate the interest
     const year = new Date().getFullYear() + parseInt(years); 
-    result.innerHTML = ""
-    
-    result.innerHTML+= `
-    <div>
-        If you deposit <b>${principal}</b>,<br>
-        at an interest rate of <b>${rate}%</b>.<br>
-        You will receive an amount of <b>${interest}</b>,<br>
-        in the year <b>${year}</b>.
-    </div>
-    `
-    console.log(interest)
+    //Obtain the year to calculate the interest by means of the Date object
+    if (principal < 0) {
+        alert(`Enter a positive number`)
+    } else { //If the amount entered is negative it triggers the alert, otherwise it shows the interest and other values inserting them in the HTML
+        result.innerHTML = ""
+        result.innerHTML+= `
+        <div class="yellow-bc">
+            If you deposit <span>${principal}</span>,<br>
+            at an interest rate of <span>${rate}%</span>.<br>
+            You will receive an amount of <span>${interest}</span>,<br>
+            in the year <span>${year}</span>.
+        </div>
+        `
+    }
 }
-// function compute()
-// {
-//     p = document.getElementById("principal").value;
-    
-// }
