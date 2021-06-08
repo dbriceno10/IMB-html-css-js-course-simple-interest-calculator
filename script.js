3bbcba996a460f00a09bbc61b0f213c61e9bc6fb
nombre <daniel.b100726@gmail.com>
2021-06-08 18:33:32 -0400

    funcional prototype

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+// const principal = document.getElementById("principal").value;
+// const rate = document.getElementById("rate").value;
+// const years = document.getElementById("years").value;
+// const interest = principal * years * rate / 100
+// const year = new Date().getFullYear() + parseInt(years);
+
+const updateRate = () => {
+    const rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
 }
-        
+
+const compute = () => {
+    const result = document.getElementById("result")
+    const principal = document.getElementById("principal").value;
+    const rate = document.getElementById("rate").value;
+    const years = document.getElementById("years").value;
+    const interest = principal * years * rate / 100
+    const year = new Date().getFullYear() + parseInt(years); 
+    result.innerHTML = ""
+    
+    result.innerHTML+= `
+    <div>
+        If you deposit <b>${principal}</b>,<br>
+        at an interest rate of <b>${rate}%</b>.<br>
+        You will receive an amount of <b>${interest}</b>,<br>
+        in the year <b>${year}</b>.
+    </div>
+    `
+    console.log(interest)
+}
+// function compute()
+// {
+//     p = document.getElementById("principal").value;
+    
+// }
